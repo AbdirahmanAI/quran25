b2d6ad9eb62020fd6ce98efc537434cf0ac5f2e9
AbdirahmanAI <gobmedia2@gmail.com>
2024-12-19 21:03:58 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://myquran.so',
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://qurankareem.app',
   generateRobotsTxt: true,
   exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
     additionalSitemaps: [
-      'https://myquran.so/server-sitemap.xml',
+      'https://qurankareem.app/server-sitemap.xml',
     ],
   },
 }
